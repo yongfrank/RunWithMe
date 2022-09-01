942070eba152bbdc1c64571ef775c5f7f5fa863c
Frank Chu <yongfrank@outlook.com>
2022-09-01 13:27:48 +0800

    0.1.0 Build 2

---
+++ Running/View/RunningData/ChartView.swift
@@ -0,0 +1,16 @@
+//
+//  ChartView.swift
+//  Running
+//
+//  Created by Frank Chu on 8/31/22.
+//
+
+import SwiftUI
+
+
+
+//struct ChartView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ChartView()
+//    }
+//}
